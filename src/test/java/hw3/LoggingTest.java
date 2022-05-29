1db6186e7a463c7c2ecc0f0bd2c35754b29d3226
lylova11 <nemova94@mail.ru>
2022-05-29 14:19:49 +0300

    hw3

--- src/test/java/hw3/LoggingTest.java
+++ src/test/java/hw3/LoggingTest.java
@@ -1,4 +1,14 @@
 package hw3;
 
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+
 public class LoggingTest {
+
+    @BeforeAll
+    static void setUp(){
+
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+
+    }
 }
